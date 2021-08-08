bca6924387c3c74763a4fa27379e4bdd0364e202
Robert.Yang <ysb05222@naver.com>
2021-08-08 11:00:11 +0900

    POST API
    POST API

---
+++ post/README.md
@@ -0,0 +1,16 @@
+## POST API
+
+  <img width="1149" alt="스크린샷 2021-08-08 오전 9 11 20" src="https://user-images.githubusercontent.com/18282470/128617428-a5596c99-84f6-49a6-8196-bee3561de195.png">
+
+|                 |                                   |
+| --------------- | --------------------------------- |
+| @RestController | Rest API 설정                     |
+| @RequestMapping | 리소스를 설정 (method로 구분가능) |
+
+|               |                           |
+| ------------- | ------------------------- |
+| @PostMapping  | Post Resource 설정        |
+| @RequestBody  | Request Body부분 Parsing  |
+| @PathVariable | URL Path Variable Parsing |
+| @JsonProperty | json naming               |
+| @JsonNaming   | class json naming         |
---
+++ post/src/main/java/me/sungbin/post/controller/PostApiController.java
@@ -0,0 +1,28 @@
+package me.sungbin.post.controller;
+
+import me.sungbin.post.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody Map<String, Object> requestData) {
+
+        requestData.forEach((key, value) -> {
+            System.out.println("key: " + key);
+            System.out.println("value: " + value);
+        });
+    }
+
+    @PostMapping("/posts")
+    public void posts(@RequestBody PostRequestDto requestDto) {
+        System.out.println(requestDto);
+    }
+}
