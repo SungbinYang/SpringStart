820701392bbd6d3da5ca6709aed34fa7b2514ce2
Robert.Yang <ysb05222@naver.com>
2021-08-10 21:12:45 +0900

    Spring Boot Exception Handling
    Spring Boot Exception Handling

---
+++ exception/src/test/java/me/sungbin/exception/ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sungbin.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
