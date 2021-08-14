4da5a6836a467c74a3d34997751681b26218c167
Robert.Yang <ysb05222@naver.com>
2021-08-14 23:38:10 +0900

    Swagger 설정
    Swagger 설정

---
+++ swagger/src/main/java/me/sungbin/swagger/SwaggerApplication.java
@@ -0,0 +1,13 @@
+package me.sungbin.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApplication.class, args);
+	}
+
+}
---
+++ swagger/src/main/java/me/sungbin/swagger/dto/UserRes.java
@@ -0,0 +1,18 @@
+package me.sungbin.swagger.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRes {
+
+    @ApiModelProperty(value = "사용자의 이름", example = "robert", required = true)
+    private String name;
+
+    @ApiModelProperty(value = "사용자의 나이", example = "26", required = true)
+    private int age;
+}
