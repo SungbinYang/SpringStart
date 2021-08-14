037111beff41a35688014872a4fd90f10945ffc3
Robert.Yang <ysb05222@naver.com>
2021-08-14 16:38:31 +0900

    naver 지역 검색 api 연동
    naver 지역 검색 api 연동

--- server-client/server/src/main/java/me/sungbin/server/controller/ServerApiController.java
+++ server-client/server/src/main/java/me/sungbin/server/controller/ServerApiController.java
@@ -4,12 +4,56 @@ import lombok.extern.slf4j.Slf4j;
 import me.sungbin.server.dto.Req;
 import me.sungbin.server.dto.User;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    // https://openapi.naver.com/v1/search/local.json
+    // ?query=%EC%A3%BC%EC%8B%9D
+    // &display=10
+    // &start=1
+    // &sort=random
+    @GetMapping("/naver")
+    public String naver() {
+
+        String query = "맨유";
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", "중국집")
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode(StandardCharsets.UTF_8)
+                .build()
+                .toUri();
+
+        log.info("uri: {}", uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "vfdlpc83HMTttlHa7Ez9")
+                .header("X-Naver-Client-Secret", "4c3TOBnBXi")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age) {
