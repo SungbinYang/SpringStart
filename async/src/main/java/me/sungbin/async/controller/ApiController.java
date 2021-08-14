7ef8f8ab4ca0722d402bf6b1670634312af54369
Robert.Yang <ysb05222@naver.com>
2021-08-14 13:03:15 +0900

    비동기 처리
    비동기 처리

---
+++ async/src/main/java/me/sungbin/async/controller/ApiController.java
@@ -0,0 +1,25 @@
+package me.sungbin.async.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.sungbin.async.service.AsyncService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ApiController {
+
+    private final AsyncService asyncService;
+
+    @GetMapping("/hello")
+    public CompletableFuture hello() {
+        log.info("completable future init'");
+        return asyncService.run();
+    }
+}
